bea39f9bf6efc6aa7f5a3c7e9081fab53cacb7ea
Nikita Rudenko <rdnkta@gmail.com>
2020-02-01 23:46:40 +0200

    Interacting with DOM elements

--- 2. Drag&Drop app/src/app.ts
+++ 2. Drag&Drop app/src/app.ts
@@ -1,7 +1,24 @@
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjustedDecriptor: PropertyDescriptor = {
+      configurable: true,
+      enumerable: true,
+      get() {
+          const boundFn = originalMethod.bind(this);
+          return boundFn;
+      }
+  }
+
+  return adjustedDecriptor;
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
@@ -9,11 +26,28 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+
+  @Autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler)
   }
 }
 
