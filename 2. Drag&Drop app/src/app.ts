56f9693356ea5fa36c00dbf6b0d0ebae0049cda2
Nikita Rudenko <rdnkta@gmail.com>
2020-02-02 16:10:56 +0200

    Filtering Projects with Enums

--- 2. Drag&Drop app/src/app.ts
+++ 2. Drag&Drop app/src/app.ts
@@ -122,7 +122,11 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter(project => {
+        if (this.type === 'active') return project.status === ProjectStatus.Active;
+        return project.status === ProjectStatus.Finished;
+      });
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     })
 
@@ -132,6 +136,8 @@ class ProjectList {
 
   private renderProjects() {
     const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+    listEl.innerHTML = '';
+
     for (const projectItem of this.assignedProjects) {
       const listItem = document.createElement('li');
       listItem.textContent = projectItem.title;
