8f2a8c5e4ef5c2b85f6119a0fb10f7796edc4330
Nikita Rudenko <rdnkta@gmail.com>
2020-02-02 17:43:18 +0200

    Rendering Project Items with a Class

--- 2. Drag&Drop app/src/app.ts
+++ 2. Drag&Drop app/src/app.ts
@@ -136,6 +136,35 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract render(): void;
 }
 
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+
+  get people() {
+    if (this.project.people === 1) {
+      return '1 person';
+    } else {
+      return `${this.project.people} people`;
+    }
+  }
+  
+  
+  constructor(hostId: string, project: Project){
+    super('single-project', hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.render();
+  }
+
+  configure(){};
+
+  render(){
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = this.people + " assigned";
+    this.element.querySelector('p')!.textContent = this.project.description;
+  };
+}
+
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
 
@@ -170,9 +199,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     listEl.innerHTML = '';
 
     for (const projectItem of this.assignedProjects) {
-      const listItem = document.createElement('li');
-      listItem.textContent = projectItem.title;
-      listEl.appendChild(listItem);
+      new ProjectItem(this.element.querySelector('ul')!.id, projectItem);
     }
   }
 }
